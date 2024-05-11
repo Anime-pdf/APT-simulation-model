fe5b128b8c701cb0361f89584b7180354f0a7d9f
Paul <paul.turkovskiy@gmail.com>
2024-05-11 21:51:46 +0300

    add sync B signal

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@ newbie uni project
 
 APT: https://en.wikipedia.org/wiki/Automatic_picture_transmission
 
-Sync A signal:  
-![Sync A](img/apt_sync_a.png)
+Sync A/B signals:  
+![Sync AB](img/apt_sync.png)
 
 Transmission algorythm:  
 ![algorythm](img/algorythm.png)
--- signal_model.py
+++ signal_model.py
@@ -3,9 +3,10 @@ import numpy as np
 def generate_sync_A(Fs):
     T = 1/4160
     dt = 1/Fs
+    
     t = np.arange(0,39*T,dt)
-    n = len(t)
-    sync_A = 11*np.ones(n)
+    
+    sync_A = 11*np.ones(len(t))
     array_0_1 = (t >= 4*T) * (t <= 6*T) + \
     (t >= 8*T) * (t <= 10*T) + \
     (t >= 12*T) * (t <= 14*T) + \
@@ -16,3 +17,20 @@ def generate_sync_A(Fs):
     
     sync_A[array_0_1] = 244
     return sync_A
+
+def generate_sync_B(Fs):
+    T = 1/4160
+    dt = 1/Fs
+    
+    t = np.arange(0,39*T,dt)
+    
+    sync_B = 11*np.ones(len(t))
+    array_0_1 = (t >= 4*T) * (t <= 7*T) + \
+    (t >= 9*T) * (t <= 12*T) + \
+    (t >= 14*T) * (t <= 17*T) + \
+    (t >= 19*T) * (t <= 22*T) + \
+    (t >= 24*T) * (t <= 27*T) + \
+    (t >= 29*T) * (t <= 32*T) + \
+    (t >= 34*T) * (t <= 37*T)
+    
+    sync_B[array_0_1] = 244
